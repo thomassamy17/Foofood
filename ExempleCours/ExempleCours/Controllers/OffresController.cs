bb8cf00a78d7946eaabb37873dae75d36cb48871
HuuTai33 <44099208+HuuTai33@users.noreply.github.com>
2019-11-28 15:30:37 +0100

    Huutai
    liste et details
    
    Co-Authored-By: Thomas <thomassamy17@users.noreply.github.com>

--- ExempleCours/ExempleCours/Controllers/OffresController.cs
+++ ExempleCours/ExempleCours/Controllers/OffresController.cs
@@ -4,38 +4,81 @@ using System.Linq;
 using System.Threading.Tasks;
 using ExempleCours.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Dapper;
 
 
 namespace ExempleCours.Controllers
 {
     public class OffresController : Controller
     {
-        private List<Offre> offres = new List<Offre>()
-        {
-            new Offre(1,"Brownie","Au chocolat","https://static.750g.com/images/622-auto/5df6e20eacc62872a3e1640305892963/brownie-au-chocolat-noir.jpeg",12),
-            new Offre(2,"Brownie blanc","Au chocolat blanc","https://img-3.journaldesfemmes.fr/0pzfJYdUsyk9N0uqNmE5Hl37RM0=/750x/smart/image-icu/10025520_1038048759.jpg",14),
-            new Offre(3,"Brownie vanille","Au chocolat vanille","https://assets.afcdn.com/recipe/20130122/61722_w1024h768c1cx826cy952.jpg",11)
-        };
         public IActionResult Index()
         {
-            
-            return View(offres);
-        }
-        public IActionResult Detail(int id)
-        {
-            Offre monOffre = null;
-            if (offres != null)
+            var chaineConnexion = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = D:\Program .NET\Foofood-master\ExempleCours\ExempleCours\Data\Data.mdf; Integrated Security = True; Connect Timeout = 30";
+
+            try
             {
-                foreach (Offre offre in offres)
+                using (var connection = new SqlConnection(chaineConnexion))
                 {
-                    if (offre.Id == id)
+                    var galerie = connection.Query<OffreEntite>("SELECT * FROM Offre");
+
+                    return View(galerie);
+
+                } // Appel du Dispose de connection (ferme la connexion)
+            }
+            catch (SqlException e)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(OffreEntite nouveau)
+        {
+            if (ModelState.IsValid)
+            {
+                var chaineConnexion = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = D:\Program .NET\Foofood-master\ExempleCours\ExempleCours\Data\Data.mdf; Integrated Security = True; Connect Timeout = 30";
+
+                try
+                {
+                    using (var connection = new SqlConnection(chaineConnexion))
                     {
-                        monOffre = offre;
+                        connection.Execute("INSERT INTO Offre (Titre,Description,Url,Prix,UserId) VALUES (@Titre,@Description,@Url,@Prix,1)",nouveau);
+
+                        return RedirectToAction(nameof(Index));
+
                     }
                 }
-
+                catch (SqlException e)
+                {
+                    return RedirectToAction(e.Message, "Home");
+                }
             }
-            return View(monOffre);
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var chaineConnexion = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = D:\Program .NET\Foofood-master\ExempleCours\ExempleCours\Data\Data.mdf; Integrated Security = True; Connect Timeout = 30";
+
+            try
+            {
+                using (var connection = new SqlConnection(chaineConnexion))
+                {
+                    var galerie = connection.QueryFirst<OffreEntite>("SELECT * FROM Offre WHERE Id = @Id",new { Id = id });
+
+                    return View(galerie);
+
+                } // Appel du Dispose de connection (ferme la connexion)
+            }
+            catch (SqlException e)
+            {
+                return RedirectToAction("Error", "Home");
+            }
         }
     }
 }
---
+++ ExempleCours/ExempleCours/Models/OffreEntite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExempleCours.Models
+{
+    public class OffreEntite
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(256), MinLength(2)]
+        public string Titre{ get;set; }
+        [Required]
+        [StringLength(256), MinLength(2)]
+        public string Description { get; set; }
+        [Required]
+        [StringLength(256), MinLength(2)]
+        public string Url { get; set; }
+        [Required]
+        public int Prix { get; set; }
+    }
+}
